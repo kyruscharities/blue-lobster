44f3b88651a169639c9c65fffb3b3c5bf7fe46fe
Chris Rothe <chris@redcanary.co>
2014-12-08 13:16:14 -0700

    add default index view

--- app/controllers/job_types_controller.rb
+++ app/controllers/job_types_controller.rb
@@ -1,4 +1,10 @@
 class JobTypesController < ApplicationController
   load_and_authorize_resource
   inherit_resources
+
+  private
+
+  def job_type_params
+    params.require(:job_type).permit(:name, :description)
+  end
 end
