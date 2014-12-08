ce354a933f6b3413f71a2be14589f0c70a64759d
Brian Beyer <brianebeyer@gmail.com>
2014-12-08 15:45:40 -0500

    looking and feeling closer matching to website

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
+  get 'admin/update_styles'
+
   root to: 'visitors#index'
   devise_for :users
 
   resources :questions
+
   resources :users do
     member do
       get :recommendations
     end
   end
+
   resources :job_types
+
   resources :job_programs
+
+  namespace :admin do
+    get :update_styles
+  end
 end
