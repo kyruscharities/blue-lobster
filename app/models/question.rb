a0dbdffdf538933212d52510619d0d0aceb1578c
Matt Green <mattgreenrocks@gmail.com>
2014-12-08 11:37:22 -0500

    Add validations for Answer, Question, SkillValue

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ActiveRecord::Base
   has_many :skill_values
   has_many :skills, through: :skill_values
+
+  validates :question, presence: true
+  validates :skills, length: { minimum: 1 }
 end
