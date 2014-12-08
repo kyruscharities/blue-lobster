db4cb42d44f5d2f7a940386f26548226869a2fdc
Jake Myers <jacob.myers@kyrus-tech.com>
2014-12-08 13:46:01 -0700

    Answers -> JobTypes!!!!!

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,38 @@ class User < ActiveRecord::Base
 
   has_many :answers
   after_create :default_role
+  
+  def get_skill_buckets
+    skill_buckets = Hash.new 0
+    
+    for ans in self.answers
+      p ans
+
+      for val in ans.question.skill_values
+        p val
+        skill_buckets[val.skill] += (ans.score * val.weight)
+      end
+    end
+
+    return skill_buckets
+  end
+
+  def get_job_types
+    job_buckets = Hash.new 0
+    skill_buckets = self.get_skill_buckets 
+
+    for jt in JobType.all
+      p jt
+      p jt.skills
+      
+      for skill in jt.skills
+        p skill
+        job_buckets[jt] += skill_buckets[skill]
+      end
+    end
+
+    return job_buckets 
+  end
 
   private
 
