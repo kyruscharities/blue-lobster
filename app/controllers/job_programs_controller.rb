570544a2bf05d87e05e09b82899082d72df807ed
Jacob Baines <jacob.baines@kyrus-tech.com>
2014-12-08 16:47:49 -0500

    Provide information about types and skills on the job program view.

--- app/controllers/job_programs_controller.rb
+++ app/controllers/job_programs_controller.rb
@@ -3,5 +3,14 @@ class JobProgramsController < ApplicationController
     inherit_resources
 
     def index
+        @job_types_by_id = {};
+        @job_skills_by_id = {};
+        @job_programs.each do |program|
+            @job_types_by_id[program.id] = program.job_types
+            @jobs = program.job_types
+            @jobs.each do |job_types|
+                @job_skills_by_id[program.id] = job_types.skills
+            end
+        end
     end
 end
