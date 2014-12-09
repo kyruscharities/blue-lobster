454a7ccdd22bd757c3c3ce8679d16212d194ebab
Jacob Baines <jacob.baines@kyrus-tech.com>
2014-12-09 09:02:03 -0500

    Added job types and skills to program overview

--- app/controllers/job_programs_controller.rb
+++ app/controllers/job_programs_controller.rb
@@ -4,13 +4,8 @@ class JobProgramsController < ApplicationController
 
     def index
         @job_types_by_id = {};
-        @job_skills_by_id = {};
         @job_programs.each do |program|
             @job_types_by_id[program.id] = program.job_types
-            @jobs = program.job_types
-            @jobs.each do |job_types|
-                @job_skills_by_id[program.id] = job_types.skills
-            end
         end
     end
 end
