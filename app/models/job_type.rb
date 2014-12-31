98f423c3d2f32914773fc62ad551ef994364e80c
Brian Beyer <brianebeyer@gmail.com>
2014-12-31 11:59:02 -0700

    another try

--- app/models/job_type.rb
+++ app/models/job_type.rb
@@ -1,6 +1,7 @@
 class JobType < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :description
+
   has_and_belongs_to_many :skills
-  has_and_belongs_to_many :programs
+  has_and_belongs_to_many :programs, join_table: 'job_types_programs'
 end
